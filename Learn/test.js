309813a2d72aa58ef8aa68bafe9a6d3af3c051b8
maddy1926 <79394400+maddy1926@users.noreply.github.com>
2025-04-16 05:28:08 +0000

    test

---
+++ Learn/test.js
@@ -0,0 +1,36 @@
+// const accountId = 639736701
+// let accountEmail = "manav@google.com"
+// let accountPassword = "12345"
+// let accountCity = "Noida"
+// let accountState;
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+// console.log();
+
+// const Name = "Manav Dixit";
+// let Number = "6397136701";
+// let EmailId = "Manavdixit@google.com";
+// City = "Etah";
+// let State;
+
+// console.table([Name, Number, EmailId, City, State]);
+
+// let score = 30;
+
+// console.log(typeof score);
+
+// let isloggedIn = 0
+
+// let BooleanisloggedIn = Boolean(isloggedIn)
+
+// console.log(BooleanisloggedIn);
+
+
+// let manav = 34 ;
+
+// let Stringmanav = String(manav);
+
+// console.log(Stringmanav);
+// console.log(typeof Stringmanav);
